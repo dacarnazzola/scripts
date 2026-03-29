d5b7a6fecc0343b94de94820f6fdd8e5ebb2ddf1
Derick Carnazzola <maxediv@gmail.com>
2026-03-29 18:47:10 -0500

    added list of homebrew-installed applications

---
+++ brew-apps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+brew install --cask discord
+brew install --cask firefox
+brew install --cask ghostty
+brew install --cask obsidian
+brew install --cask vlc
+
+brew install gcc
+brew install git
+brew install mactop
+brew install ninja
+brew install vim
+brew install cmake
+brew install llama.cpp
+brew install pyenv
